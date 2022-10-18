0440252631772dd11dc3b32c1f7a65ca7cdcc5d5
Taka005 <01taka005@gmail.com>
2022-10-18 14:45:10 +0900

    修正

--- connect.js
+++ connect.js
@@ -8,17 +8,84 @@ form.addEventListener("submit", (event)=>{
     for(i=0;i<lists.length;i++){
        lists[i].remove()
     }
-    const token = input.value;
-  
-
-
-
-    for(i=0;i<temp;i++){
-        const random = Math.floor(Math.random() * temp_data.length);
-        const li = document.createElement("li");
-        li.innerText = temp_data[random];
-        li.classList.add("list-group-item")  
-
-        ul.appendChild(li);
-    }
+    
+    websocket()
 });
+
+function websocket(){
+    const token = input.value;
+
+    const ws = new websocket("wss://ugc.renorari.net/api/v1/gateway");
+
+    ws.addEventListener("close", (code, reason)=>{
+      setTimeout(() =>{
+        websocket();
+      }, 10000);
+      const li = document.createElement("li");
+      li.innerText = `LOG:${code} ${reason}`;
+      li.classList.add("list-group-item")  
+      ul.appendChild(li);
+    });
+    
+    ws.addEventListener("error", (error)=>{
+        const li = document.createElement("li");
+        li.innerText = `LOG:${error}`;
+        li.classList.add("list-group-item")  
+        ul.appendChild(li);
+        return;
+    });
+
+    ws.addEventListener("message", (rawData)=>{
+
+      Zlib.inflate(rawData, (err,_data) =>{
+        if(!err){
+            const li = document.createElement("li");
+            li.innerText = `LOG:${err}`;
+            li.classList.add("list-group-item")  
+            ul.appendChild(li);
+            return;
+        }
+        let data = JSON.parse(_data);
+        if(data.type === "hello"){
+          ws.send(Zlib.deflateSync(JSON.stringify({
+            "type": "identify",
+            "data": {
+              "token": token
+            }
+            }),(err)=>{
+              if(!err) return; 
+              const li = document.createElement("li");
+              li.innerText = `LOG:${err}`;
+              li.classList.add("list-group-item")  
+              ul.appendChild(li);
+            }
+          ));
+        }else if(data.type === "message"){
+          const msg = data.data.data
+
+          const li = document.createElement("li");
+          li.innerText =`${msg.author.username}#${msg.author.discriminator}:${msg.message.content}`;
+          li.classList.add("list-group-item")  
+          ul.appendChild(li);
+
+        }else if(data.type === "identify"){
+          if(!data.success){
+            const li = document.createElement("li");
+            li.innerText = "LOG:No Ready";
+            li.classList.add("list-group-item")  
+            ul.appendChild(li);
+            return;
+          }
+          const li = document.createElement("li");
+          li.innerText = "LOG:Ready";
+          li.classList.add("list-group-item")  
+          ul.appendChild(li);
+          return;
+
+        }else if(data.type === "heartbeat"){
+          return
+        }
+      });
+    });
+  }
+  websocket()
