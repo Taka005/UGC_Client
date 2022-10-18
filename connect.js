2bd29e5887bdde3b4125eb30564545698cea56fc
Taka005 <01taka005@gmail.com>
2022-10-18 21:58:22 +0900

    修正

--- connect.js
+++ connect.js
@@ -43,7 +43,7 @@ async function websocket(){
   });
 
   ws.addEventListener("message", (rawData)=>{
-    const _data = new Zlib.Inflate(Uint8Array.from(rawData));
+    const _data = new Zlib.RawInflate(Uint8Array.from(rawData));
     const data = JSON.parse(_data.decompress());
     console.log(data)
     if(data.type === "hello"){
